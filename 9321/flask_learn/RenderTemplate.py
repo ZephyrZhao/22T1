b84ce02c1f33d8f3135e474ebf119f9b485c2bc6
zhaojunfeng <zhaojunfeng@4paradigm.com>
2022-02-25 12:07:09 +0800

    Firday!

--- 9321/flask_learn/RenderTemplate.py
+++ 9321/flask_learn/RenderTemplate.py
@@ -27,12 +27,24 @@ def hello_name(user):
 @app.route('/hello/<int:score>/')
 def isPass(score):
     return render_template('isPass.html',marks=score)
+# html中传入int 型的marks，用{%...%}括住if-else-endif
+
+
 
 #####淦，还有一个栗子
 @app.route('/result/')
 def result():
     dic = {'phy':50,'che':60,'maths':70}
     return render_template('result.html', result = dic)
+# html 中传入ditc型的result，{%for key,value in result.items() %}返回key和value值
+# {{...}} 括住打印到模版的 输出
+
+# --------静态文件-------------------------------------------------------------------------------------
+
+# static_file.html 中的按钮 onclick事件调用static/static_file.js中的sayHello()函数
+@app.route('/static/')
+def static_file():
+    return render_template("static_file.html")
 
 if __name__=='__main__':
     app.run(debug=True)
